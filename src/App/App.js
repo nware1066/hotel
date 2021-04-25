f336573c9236abb07498abf5e882ea6b16d836e0
Naomi Ware <nware1066@yahoo.com>
2021-04-25 11:56:22 -0600

    Create App.js file

---
+++ src/App/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import Header from '../Header/Header';
+import {Component} from 'react';
+
+class App extends Component {
+  constructor () {
+    super();
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Welcome</h1>
+      </div>
+    );
+  }
+
+}
+
+export default App;
