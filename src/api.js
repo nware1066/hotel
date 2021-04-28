fc52fa75ffb4ad812312e5f4421c57fa0fa94bc2
Naomi Ware <nware1066@yahoo.com>
2021-04-27 20:19:40 -0600

    Add fetch for booking data

--- src/api.js
+++ src/api.js
@@ -1,7 +1,27 @@
-export const fetchGuests = () => {
+function fetchGuests () {
     return fetch('http://localhost:3001/api/v1/customers')
     .then(response => response.json())
     .then(data => data.guests)
     .catch(error => console.log("fetch guest error"))
 }
 
+function fetchBookings() {
+    return fetch('http://localhost:3001/api/v1/bookings')
+    .then(response => response.json())
+    .then(data => data.bookings)
+    .catch(error => console.log("fetch bookings error"))
+}
+
+function getAllFetchedData() {
+    return Promise.all( [fetchGuests(), fetchBookings()] )
+    .then(response => {
+        let allData = {};
+        allData.users = response[0];
+        allData.bookings = response[1];
+        return allData;
+    })
+    .catch(error => console.log("allData error"))
+}
+
+
+export default getAllFetchedData;
