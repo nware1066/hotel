5ab6b55b17df872d07cc696df6bfaad28c1a3936
Naomi Ware <nware1066@yahoo.com>
2021-04-27 20:21:01 -0600

    Create App class, add state and componentDidMount for booking data

--- src/App/App.js
+++ src/App/App.js
@@ -1,22 +1,39 @@
 import './App.css';
 import Header from '../Header/Header';
 import {Component} from 'react';
+import getAllFetchedData from '../api.js';
+import Dashboard from '../Dashboard/Dashboard';
 
 class App extends Component {
-  constructor () {
+  constructor() {
     super();
     this.state = {
+      guest: {
+        userName: "Faustino Quitzon",
+        id: 9,
+      },
+      bookings: [],
+      totalSpent: 0,
+      error: "",
     };
+  }
+
+  componentDidMount() {
+    getAllFetchedData()
+      .then((allData) => this.setState({ bookings: allData.bookings }))
+      .catch((error) => {
+        this.setState({ error: error });
+      });
   }
 
   render() {
     return (
       <div className="App">
-        <Header /> 
+        <Header />
+        <Dashboard bookings={this.state.bookings} userID={this.state.guest.id} />
       </div>
     );
   }
-
 }
 
 export default App;
