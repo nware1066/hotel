46464bb82107b47d5534ef1583317c58f04e6ac1
Naomi Ware <nware1066@yahoo.com>
2021-04-29 14:40:57 -0600

    Add guest name to welcome message

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,11 +1,20 @@
 import './Header.css';
 
-function Header() {
+function Header(props) {
+
+
+    let guestName;
+
+    if (props.name) {
+        guestName = props.name;
+    }
+
     return (
         <div className="header-wrapper">
-            <h1>Welcome to Travel Home Hotel</h1>
+            <h1>{`Welcome to Travel Home Hotel ${guestName}`}</h1>
         </div>
     )
+
 }
 
 export default Header;
